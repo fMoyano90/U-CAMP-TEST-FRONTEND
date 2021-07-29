f210ce7caac73e6baf4415ebc85f42edec035ed4
Felipe Moyano <f.moyano90@gmail.com>
2021-07-28 23:28:47 -0400

    :rocket: Change API URL of products for production

--- src/helpers/get-products.js
+++ src/helpers/get-products.js
@@ -1,5 +1,5 @@
 export const getProducts = async (query) => {
-  const url = `http://localhost:5000/api/search?query=${encodeURI(query)}`;
+  const url = `https://u-camp-test-backend.herokuapp.com/api/search?query=${encodeURI(query)}`;
   const resp = await fetch(url);
   return await resp.json();
 };
